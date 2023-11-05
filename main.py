faf140456469ddbb806988b3a67436ac8abbc983
justynapokora <justynapokora@icloud.com>
2023-11-05 01:59:08 +0100

    add options for clique distance and subgraph

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-g1', '--graph1')
+    parser.add_argument('-c', '--clique', action='store_true')
+
     parser.add_argument('-g2', '--graph2')
+    parser.add_argument('-d', '--distance', action='store_true')
+    parser.add_argument('-s', '--subgraph', action='store_true')
 
     args = parser.parse_args()
 
@@ -18,6 +22,28 @@ if __name__ == '__main__':
         g1.print()
         graph = get_graph_from_multigraph(g1)
         undirected_graph = get_undirected_graph_from_directed_graph(MultiDiGraph(graph))
-        
+
     else:
         print('No graph data file given!')
+        exit()
+
+    if args.clique:
+        print("Maximal clique for graph 1: ")
+        pass
+
+    if args.graph2:
+        # with open(str(sys.argv[1]), 'r') as f:
+        g2 = MultiDiGraph(read_graph_from_file(args.graph2))
+        g2.print()
+    else:
+        if args.distance or args.subgraph:
+            print('No graph data file for 2nd graph given!')
+        exit()
+
+    if args.distance:
+        print("Distance between graph 1 and graph 2:")
+        pass
+
+    if args.subgraph:
+        print("Maximal subgraph for graph 1 and graph 2:")
+        pass
